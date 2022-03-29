0f5583573b07a8234d84f68411c2477f428b447c
Lula <lula@42sp.org.br>
2022-03-29 10:27:52 -0300

    WIP

--- tcr_quarenta_e_duolingo/negocio/quarenta_e_duolingo.py
+++ tcr_quarenta_e_duolingo/negocio/quarenta_e_duolingo.py
@@ -1,2 +1,7 @@
 def verificarPTBR(palavra_para_traducao, palavra_que_vai_ser_traduzida):
     return palavra_para_traducao == palavra_que_vai_ser_traduzida
+
+def mensagem(resultado):
+    if (resultado == True):
+        print("Você acertou!")
+    print("Errrrrou!!!!")

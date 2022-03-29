f6052bd668da63b01b584a007335744aad98188a
Lula <lula@42sp.org.br>
2022-03-29 10:20:51 -0300

    WIP

--- tcr_quarenta_e_duolingo/negocio/quarenta_e_duolingo.py
+++ tcr_quarenta_e_duolingo/negocio/quarenta_e_duolingo.py
@@ -1,2 +1,4 @@
-def somar(a, b):
-    return a + b
+def verificarPTBR(palavra_para_traducao, palavra_que_vai_ser_traduzida):
+    if (palavra_para_traducao == palavra_que_vai_ser_traduzida):
+        return True
+    return False
--- tcr_quarenta_e_duolingo/testes/testes_quarenta_e_duolingo.py
+++ tcr_quarenta_e_duolingo/testes/testes_quarenta_e_duolingo.py
@@ -4,10 +4,10 @@ import negocio.quarenta_e_duolingo as negocio
 class TesteDeExemplo(TestCase):
 
     def teste_de_exemplo(self):
-        a = 20
-        b = 25
-        esperado = 45
+        palavra_apresentada = "quarenta e dois"
+        palavra_inserida = "forty two"
+        resposta_certa = "forty two"
 
-        calculado = negocio.somar(a, b)
+        resultado = negocio.verificarPTBR(palavra_inserida, resposta_certa)
 
-        self.assertEqual(esperado, calculado)
+        self.assertTrue(resultado)

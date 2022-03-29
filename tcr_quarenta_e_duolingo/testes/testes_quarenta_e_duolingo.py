54dc3a7d60d3813f828d612088d4d25d62d9c9fe
Lula <lula@42sp.org.br>
2022-03-29 10:24:07 -0300

    WIP

--- tcr_quarenta_e_duolingo/testes/testes_quarenta_e_duolingo.py
+++ tcr_quarenta_e_duolingo/testes/testes_quarenta_e_duolingo.py
@@ -11,3 +11,12 @@ class TesteTraducao(TestCase):
         resultado = negocio.verificarPTBR(palavra_inserida, resposta_certa)
 
         self.assertTrue(resultado)
+
+    def teste_palavra_inserida_nao_eh_a_palavra_certa(self):
+        palavra_apresentada = "quarenta e dois"
+        palavra_inserida = "fort two"
+        resposta_certa = "forty two"
+
+        resultado = negocio.verificarPTBR(palavra_inserida, resposta_certa)
+
+        self.assertFalse(resultado)
